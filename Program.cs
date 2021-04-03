af20ee9fe6659939c91241bea7efea258d505377
Robert Stimmel <B.Stimmel80@gmail.com>
2021-04-03 01:21:41 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,8 +17,12 @@ namespace Tri_Wizard_Interview_Task_Script
 
     class TotalPerimeter 
     {
-        float TotalP(List<IShapes> shapes)
+        float TotalP()
         { 
+            List<float> Shapes = new List<float>();
+
+            Shapes.Add();
+
             float totalPerimeter = 0;
 
             foreach(var shape in Shapes)
